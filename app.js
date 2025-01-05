cc6db1993d29d6351d275fb96011b63b7fea238e
Malaysia Thomas <105211177+MalaysiaThomas@users.noreply.github.com>
2025-01-05 17:42:34 -0500

    Insert desired city names into listed city slots

---
+++ app.js
@@ -0,0 +1,14 @@
+let rowOneCity = document.getElementById("row-one-city-name")
+let rowTwoCity = document.getElementById("row-two-city-name")
+let rowThreeCity = document.getElementById("row-three-city-name")
+let rowFourCity = document.getElementById("row-four-city-name")
+let rowFiveCity = document.getElementById("row-five-city-name")
+let rowSixCity = document.getElementById("row-six-city-name")
+
+// Insert desired city names into listed city slots
+rowOneCity.innerHTML = "New York"
+rowTwoCity.innerHTML = "London"
+rowThreeCity.innerHTML = "Paris"
+rowFourCity.innerHTML = "Bangkok"
+rowFiveCity.innerHTML = "Tokyo"
+rowSixCity.innerHTML = "Sydney"
