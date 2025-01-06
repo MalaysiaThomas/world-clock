e28ca8f87d4d9397e197b0eaec4ff83685ed4db7
Malaysia Thomas <105211177+MalaysiaThomas@users.noreply.github.com>
2025-01-05 19:27:48 -0500

    add functionality to display default location

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+// Selected city information
+let selectedCityName = document.getElementById("selected-city-name");
+let selectedCityTime = document.getElementById("selected-city-current-time");
+let selectedCityDate = document.getElementById("selected-city-date");
+
+
+// Listed city name containers
 let rowOneCity = document.getElementById("row-one-city-name")
 let rowTwoCity = document.getElementById("row-two-city-name")
 let rowThreeCity = document.getElementById("row-three-city-name")
@@ -12,3 +19,15 @@ rowThreeCity.innerHTML = "Paris"
 rowFourCity.innerHTML = "Bangkok"
 rowFiveCity.innerHTML = "Tokyo"
 rowSixCity.innerHTML = "Sydney"
+
+// Load users' location information as default selected city
+function formatLocatedCityName () {
+    let userLocation = moment.tz.guess()
+    let userLocationFormatted = userLocation.replace("_", " ")
+    if (selectedCityName.innerHTML === "Locating...") {
+        selectedCityName.innerHTML = userLocationFormatted
+        selectedCityTime.innerHTML = moment().format('hh:mm A')
+        selectedCityDate.innerHTML = moment().format('dddd, MMMM Do YYYY');
+    }
+}
+formatLocatedCityName()
